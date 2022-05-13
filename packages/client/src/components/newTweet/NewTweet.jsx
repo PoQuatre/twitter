edec4f801f8f034ad1ba348270b49e26319d075c
Kar1004 <97450951+Kar1004@users.noreply.github.com>
2022-05-13 17:21:18 +0200

    feat(client): add a tweetmessage

--- packages/client/src/components/newTweet/NewTweet.jsx
+++ packages/client/src/components/newTweet/NewTweet.jsx
@@ -2,38 +2,67 @@ import Button from 'react-bootstrap/Button';
 import React, { useState } from 'react';
 import styles from './newTweet.module.css';
 import Col from 'react-bootstrap/Col';
+import Logo from '../Logo';
 
 function NewTweet() {
-  const [tweetMessage, setTweetMessage] = useState('');
+  const [tweetMessage, setTweetMessage] = useState([{ id: 1, value: 'check' }]);
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
   const sendTweet = (e) => {
     e.preventDefault();
-    setTweetMessage('');
+    if (input !== '') {
+      console.log('na');
+      const tweetDetails = {
+        id: Math.floor(Math.random() * 1000),
+        value: input,
+      };
+      setInput('');
+      setTweetMessage((messages) => [tweetDetails, ...messages]);
+    }
+  };
+
+  const deleteTweet = (e, id) => {
+    e.preventDefault();
+    setTweetMessage(tweetMessage.filter((tweet) => tweet.id != id));
   };
 
   return (
     <Col md={6}>
       <div className={styles.tweetbox}>
-        <form>
+        <form onSubmit={(e) => sendTweet(e)}>
           <div className=" border-bottom">
             <h1>home</h1>
           </div>
           <div className={styles.tweetboxInput}>
             <input
-              value={tweetMessage}
-              onChange={(e) => setTweetMessage(e.target.value)}
+              value={input}
+              onChange={(e) => handleChange(e)}
               placeholder="Prosternez vous et écoutez moi?"
               type="text"
             />
           </div>
-          <Button
-            onClick={sendTweet}
-            type="submit"
-            className={styles.tweetboxButton}
-          >
+          <Button type="submit" className={styles.tweetboxButton}>
             Tweet
           </Button>
         </form>
+      </div>
+      <div>
+        {tweetMessage.map((tweet) => (
+          <div className=" d-flex flex-column">
+            <div className="tweetMessage">{tweet.value}</div>
+            <Button
+              type="submit"
+              className={styles.tweetboxButton}
+              onClick={(e) => deleteTweet(e, tweet.id)}
+            >
+              x
+            </Button>
+          </div>
+        ))}
       </div>
     </Col>
   );
